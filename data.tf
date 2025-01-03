7466fae81576cab3562d717d77c823c0da9cc93a
vidalgithub <88409846+vidalgithub@users.noreply.github.com>
2025-01-02 21:20:08 -0600

    Update data.tf

--- data.tf
+++ data.tf
@@ -57,6 +57,28 @@ data "aws_ami" "custom_ami" {
   }
 }
 
+# Data block to fetch a custom UBUNTU AMI ID in your account
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["099720109477"] # Canonical
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
 # Output the custom AMI ID - OPTIONAL
 output "custom_ami_id" {
   value = data.aws_ami.custom_ami.id
